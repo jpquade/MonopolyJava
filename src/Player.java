bf179121e67aa08bce59d5bd7416a7c3c0454793
Jeremy Quade <jeremy.quade@psware.com>
2024-06-24 15:03:05 -0700

    Added code to token assignment for each player

--- src/Player.java
+++ src/Player.java
@@ -2,37 +2,35 @@ import Enums.Token;
 
 public class Player {
 
-    // Player token choice
-    Token token;
-    int location;
-    double cash;
+    private int playerNumber;
+    private Token token;
+    private int boardLocation;
+    private double cash;
 
-    public Player(Token token, int location, double cash) {
+    public Player(int playerNumber, Token token, int boardLocation, double cash) {
+        this.playerNumber = playerNumber;
         this.token = token;
-        this.location = location;
+        this.boardLocation = boardLocation;
         this.cash = cash;
     }
 
+    public int getPlayerNumber() { return playerNumber;}
+    public void setPlayerNumber(int playerNumber) { this.playerNumber = playerNumber;}
     public Token getToken() {
         return token;
     }
-
     public void setToken(Token token) {
         this.token = token;
     }
-
-    public int getLocation() {
-        return location;
+    public int getBoardLocation() {
+        return boardLocation;
     }
-
-    public void setLocation(int location) {
-        this.location = location;
+    public void setBoardLocation(int boardLocation) {
+        this.boardLocation = boardLocation;
     }
-
     public double getCash() {
         return cash;
     }
-
     public void setCash(double cash) {
         this.cash = cash;
     }
--- src/TurnTracker.java
+++ src/TurnTracker.java
@@ -2,54 +2,102 @@ import Enums.Token;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class TurnTracker {
 
-    int numberOfPlayers;
-    ArrayList<Player> playerList = new ArrayList<>();
-    ArrayList<String> tokenList;
-    ArrayList<Integer> randomPlayerList = new ArrayList<>();
-    Token token = Token.NONE;
+    private int numberOfPlayers;
+    private final ArrayList<Player> playerList;
+    private final ArrayList<String> tokenList;
+    private ArrayList<Integer> randomPlayerList;
+    private final Scanner scanner;
 
     public TurnTracker() {
-        this.numberOfPlayers = 0;
-        this.playerList = new ArrayList<>();
-        this.tokenList = tokenList = new ArrayList<>();
-        this.randomPlayerList = randomPlayerList = new ArrayList<>();
-        this.token = Token.NONE;
+        numberOfPlayers = 0;
+        playerList = new ArrayList<>();
+        tokenList = new ArrayList<>(Arrays.asList("DOG", "CAT", "BATTLESHIP", "PENGUIN", "RUBBERDUCKY", "TOPHAT", "RACECAR", "THIMBLE"));
+        randomPlayerList = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    private boolean isInteger(String s) {
+        int radix = 10;
+
+        if(s.isEmpty()) return false;
+        for(int index = 0; index < s.length(); index++) {
+            if(index == 0 && s.charAt(index) == '-') {
+                if(s.length() == 1) return false;
+                else continue;
+            }
+            if(Character.digit(s.charAt(index),radix) < 0) return false;
+        }
+        return true;
+    }
+
+    private boolean between(String s){
+        return Integer.parseInt(s) < 2 || 8 < Integer.parseInt(s);
     }
 
     public void turnProgression(){
-        Scanner keyboard = new Scanner(System.in);
 
-        tokenList = new ArrayList<>(Arrays.asList("DOG", "CAT", "BATTLESHIP", "PENGUIN", "RUBBERDUCKY", "TOPHAT", "RACECAR", "THIMBLE"));
+        String entry = "";
 
-        System.out.println();
-        System.out.print("What is the number of players? ");
-
-        numberOfPlayers = Integer.parseInt(keyboard.nextLine());
-
-        System.out.println();
-
-        for (int i = 0; i < numberOfPlayers; i++){
+//        do{
+//            System.out.println();
+//            System.out.print("What is the number of players(Make sure to only enter a valid number \"2-8\")? ");
+//            entry = scanner.nextLine().trim();
+//        }
+//        while(!isInteger(entry) || between(entry));
+//
+//        numberOfPlayers = Integer.parseInt(entry);
+        boolean isNumber = false;
+        int value = 0;
+        while(!isInteger(entry) || between(entry)){
             System.out.println();
-            System.out.println("########################################################");
-            System.out.println(STR."Player \{i + 1}, which token do you want to use?");
-            System.out.println();
+            System.out.print("What is the number of players(Make sure to only enter a valid number \"2-8\")? ");
+            entry = scanner.nextLine().trim();
 
-            for(int j = 0; j < tokenList.size(); j++){
-                System.out.println(STR."\{j}: \{tokenList.get(j)}");
+            if(isInteger(entry)){
 
             }
+        }
+        numberOfPlayers = Integer.parseInt(entry);
+
+        System.out.println();
+
+        for (int index = 0; index < numberOfPlayers; index++){
+            System.out.println();
+            System.out.println("########################################################");
+            System.out.println(STR."Player \{index + 1}, which token do you want to use?");
+            System.out.println();
+
+            for(int tokenIndex = 0; tokenIndex < tokenList.size(); tokenIndex++){
+                System.out.println(STR."\{tokenIndex}: \{tokenList.get(tokenIndex)}");
+            }
+                int playerNumber = index + 1;
 
             System.out.println();
             System.out.print("Enter the number of the token you want to use: ");
 
-            int entry = Integer.parseInt(keyboard.nextLine());
+            int tokenNumber = Integer.parseInt(scanner.nextLine().trim());
 
-            playerList.add(new Player(Token.valueOf(tokenList.get(entry)),0,1500));
-            tokenList.remove(entry);
+            playerList.add(new Player(playerNumber, Token.valueOf(tokenList.get(tokenNumber)),0,1500));
+            tokenList.remove(tokenNumber);
+
+            System.out.println();
+            System.out.println(STR."Player \{playerNumber} is the \{playerList.get(index).getToken()}");
         }
+
+        System.out.println();
+        Iterator<Player> iterator = playerList.iterator();
+
+//        while(iterator.hasNext()){
+//            System.out.println(iterator.next() + " ");
+//        }
+        // randomize which player starts
+
+
+
     }
 }
