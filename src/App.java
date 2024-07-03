3961404f61f01bdd1f8e79af1430e3a5eda2e4f8
Jeremy <jpquade@gmail.com>
2024-07-02 22:15:14 -0700

    Shortened variable names and assigned references to objects making them easier to access

--- src/App.java
+++ src/App.java
@@ -1,10 +1,3 @@
-import Enums.Color;
-import Enums.PropertyType;
-import Enums.Token;
-import Properties.Property;
-
-import java.sql.Array;
-import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
