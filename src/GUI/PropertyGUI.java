61b65aef65420873f19028e00407006920fa5b05
Jeremy Quade <jeremy.quade@psware.com>
2024-08-19 13:54:27 -0700

    GUI added singlePropertyDisplayLabel logic

--- src/GUI/PropertyGUI.java
+++ src/GUI/PropertyGUI.java
@@ -1,23 +1,45 @@
 package GUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PropertyGUI {
 
-    public PropertyGUI(JLayeredPane rLayeredPane) {
+    public PropertyGUI(JLayeredPane rLayeredPane) throws IOException {
 
         // property button setup
-        ArrayList<JButton> propertyButtons = new ArrayList<>();
+        ArrayList<JButton> propertyButtonsList = new ArrayList<>();
         ArrayList<Integer> xList = new ArrayList<>(Arrays.asList(875,718,562,483,327,249,128,128,128,128,128,128,128,128,249,405,483,561,639,717,796,874,953,953,953,953,953,953));
         ArrayList<Integer> yList = new ArrayList<>(Arrays.asList(826,826,826,826,826,826,749,671,593,515,437,359,202,124,3,3,3,3,3,3,3,3,123,202,358,436,593,749));
+
+        // property button sizes
         int smallEdge = 77;
         int longEdge = 120;
+
+        // single property display sizes
+        int propertyScaleW = 400;
+        int propertyScaleH = 400;
+
+        // displays one property at a time for clicking on a board property
+        BufferedImage singlePropertyDisplayImage = ImageIO.read(new File("src/MonopolyImages/monopolyPropertyBlank.png"));
+        JLabel singlePropertyDisplayLabel = new JLabel(new ImageIcon(singlePropertyDisplayImage.getScaledInstance(propertyScaleW,propertyScaleH, Image.SCALE_DEFAULT)));
+        singlePropertyDisplayLabel.setBounds(400,300,propertyScaleW,propertyScaleH);
+        singlePropertyDisplayLabel.setVisible(false);
+        singlePropertyDisplayLabel.setText(singlePropertyDisplayLabel.getText() + "somethingtttttttttttttttttttttttttttttttt");
+        //singlePropertyDisplayLabel.set
+        singlePropertyDisplayLabel.setVerticalAlignment(JLabel.CENTER);
+        singlePropertyDisplayLabel.setHorizontalAlignment(JLabel.CENTER);
+        rLayeredPane.add(singlePropertyDisplayLabel);
 
         JButton overLayButton = new JButton();
         overLayButton.setBounds(0,0, 1200, 950);
@@ -30,6 +52,7 @@ public class PropertyGUI {
             public void mouseClicked(MouseEvent e) {
                 System.out.println("Overlay set to false");
                 overLayButton.setVisible(false);
+                singlePropertyDisplayLabel.setVisible(false);
             }
 
             @Override
@@ -56,10 +79,12 @@ public class PropertyGUI {
         rLayeredPane.add(overLayButton, JLayeredPane.POPUP_LAYER);
 
 
+
         for(int i = 0; i < xList.size(); i++){
             int index = i;
-            propertyButtons.add(new JButton());
-            JButton propButton = propertyButtons.get(i);
+
+            propertyButtonsList.add(new JButton());
+            JButton propButton = propertyButtonsList.get(i);
             if(i <= 5) propButton.setBounds(xList.get(i), yList.get(i), smallEdge, longEdge);
             else if(i <= 13) propButton.setBounds(xList.get(i), yList.get(i), longEdge, smallEdge);
             else if(i <= 21) propButton.setBounds(xList.get(i), yList.get(i), smallEdge, longEdge);
@@ -74,6 +99,7 @@ public class PropertyGUI {
                 public void mouseClicked(MouseEvent e) {
                     System.out.println("Overlay set to true");
                     overLayButton.setVisible(true);
+                    singlePropertyDisplayLabel.setVisible(true);
                 }
 
                 @Override
