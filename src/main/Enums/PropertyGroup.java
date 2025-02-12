6378755be50220c0bd43dedea39ccead57299b4e
jquade <jpquade@gmail.com>
2025-02-12 08:39:28 -0700

    Updated command box gui.
    Added button box gui and more buttons to select from for purchases and selections.

---
+++ src/main/Enums/PropertyGroup.java
@@ -0,0 +1,72 @@
+package main.Enums;
+
+public enum PropertyGroup {
+    BROWN,
+    LIGHTBLUE,
+    PINK,
+    ORANGE,
+    RED,
+    YELLOW,
+    GREEN,
+    DARKBLUE,
+    UTILITY,
+    RAILROAD;
+
+    enum BROWN{
+        MEDITERRANEAN_AVENUE,
+        BALTIC_AVENUE,
+    };
+
+    enum LIGHT_BLUE{
+        ORIENTAL_AVENUE,
+        VERMONT_AVENUE,
+        CONNECTICUT_AVENUE,
+    };
+
+    enum PINK{
+        ST_CHARLES_PLACE,
+        STATES_AVENUE,
+        VIRGINIA_AVENUE,
+    };
+
+    enum ORANGE{
+        ST_JAMES_PLACE,
+        TENNESSEE_AVENUE,
+        NEW_YORK_AVENUE,
+    };
+
+    enum RED{
+        KENTUCKY_AVENUE,
+        INDIANA_AVENUE,
+        ILLINOIS_AVENUE,
+    };
+
+    enum YELLOW{
+        ATLANTIC_AVENUE,
+        VENTNOR_AVENUE,
+        MARVIN_GARDENS,
+    };
+
+    enum GREEN{
+        PACIFIC_AVENUE,
+        NORTH_CAROLINA_AVENUE,
+        PENNSYLVANIA_AVENUE,
+    };
+
+    enum DARK_BLUE{
+        PARK_PLACE,
+        BOARDWALK,
+    };
+
+    enum UTILITY{
+        ELECTRIC_COMPANY,
+        WATER_WORKS,
+    };
+
+    enum RAILROAD{
+        READING_RAILROAD,
+        PENNSYLVANIA_RAILROAD,
+        B_AND_O_RAILROAD,
+        SHORT_LINE_RAILROAD,
+    };
+}
